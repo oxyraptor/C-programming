70d2406009fa086df9049acdf5bef6cce1a03aa5
oxyraptor <mandarjadhav236@gmail.com>
2026-08-23 19:00:25 +0530

    Enable float variables output in main.c

--- main.c
+++ main.c
@@ -18,12 +18,12 @@ int main() {
     printf("the year is %d", year);
     printf("you have ordered %d x items\n", quant);
 
-  // float gpa =2.5;
-  // float price = 19.99;
-  // float temp = -10.156;
-  // printf("the temp is %f\n", temp);
-  // printf("the price is $%f\n",price);
-  // printf("your gpa is %f \n", gpa);
+    float gpa =2.5;
+    float price = 19.99;
+    float temp = -10.156;
+    printf("the temp is %f\n", temp);
+    printf("the price is $%f\n",price);
+    printf("your gpa is %f \n", gpa);
 
 //   double pi = 3.141592653589793;
 //   printf("pi is approx %.15lf\n", pi);
